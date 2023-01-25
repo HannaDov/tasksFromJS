2d6fe6e403deb55cf8f9d9977141cb3af903c51f
HannaDov <dovgiallohanna@gmail.com>
2023-01-26 00:00:35 +0300

    4 lesson for ResultSchool

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const upButton=document.querySelector('.up-button')
-const downButton=document.querySelector('.down-button')
-const sidebar=document.querySelector('.sidebar')
-const mainSlide=document.querySelector('.main-slide')
-const container=document.querySelector('.container')
-const slidesCount=mainSlide.querySelectorAll('div').length
-let activeSlideIndex=0
-sidebar.style.top=`-${(slidesCount-1)*100}vh`
-upButton.addEventListener('click', ()=>{
-    changeSlide('up')
-})
-downButton.addEventListener('click', ()=>{
-    changeSlide('down')
-})
-function changeSlide(direction) {
-    if (direction==='up'){
-        activeSlideIndex++
-        if(activeSlideIndex ===slidesCount){
-            activeSlideIndex=0
-        }
-    } else if (direction==='down'){
-        activeSlideIndex--
-        if(activeSlideIndex<0){
-            activeSlideIndex=slidesCount-1
-        }
-    }
-    const height = container.clientHeight
-    mainSlide.style.transform = `translateY(-${activeSlideIndex*height}px)`
-    sidebar.style.transform = `translateY(${activeSlideIndex*height}px)`
+const board=document.querySelector('#board')
+const SQUARES_NUMBER=550
+const colors=['#18AC18','#D7D21E','#D7841E',
+    '#D7231E','#AB186E','#6B198F','#3B2494','#223F90','#137D82']
+
+for (let i=0; i<SQUARES_NUMBER; i++){
+    const square=document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', ()=>
+        setColor(square)
+    )
+    square.addEventListener('mouseleave',()=>
+        removeColor(square)
+    )
+    board.append(square)
+}
+
+function setColor(element) {
+    const color=getRandomColors()
+    element.style.backgroundColor = color
+    element.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`
+}
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow=`0 0 2px #000`
+}
+function getRandomColors() {
+    const index= Math.floor(Math.random()*colors.length)
+    return colors[index]
 }
